2c237e96dd14701d82060d0a4bf11fe4e42198bb
James Holmes <48929378+J-Holmes@users.noreply.github.com>
2021-10-14 09:14:55 -0400

    Add files via upload

---
+++ RPN.java
@@ -0,0 +1,45 @@
+/*
+Course: COP3530 RVC 1218
+1218 - Fall 2021
+
+Group 16
+James Holmes
+Carlos Aguayo
+
+*/
+
+
+package app;
+
+public class RPN {
+    
+public static void main(String[] arg) {
+    
+    String s[] =    {"5 + ) * ( 2",
+                    "2 + ( -3 * 5 )",
+                    "( ( 2 + 3 ) * 5 ) * 8",
+                    "5 * 10 + ( 15 - 20 ) ) - 25",
+                    " 5 + ( 5 * 10 + ( 15 - 20 ) - 25 ) * 9"                    
+                    };
+         
+        for (int i = 0; i < s.length; i++) {
+            Arithmetic a = new Arithmetic(s[i]);
+                System.out.println("***Equation # " + (i+1) + "***");	
+            if (a.isBalance()) {
+                System.out.println("Expression " + s[i] + " is balanced\n");
+                    a.postFixExpression();
+                    System.out.println("The post fixed expression is " + a.postFixExpression());
+                    System.out.println("");
+                    System.out.println("The expression evaluates to " + a.evaluateRPN());
+                    System.out.println("");                    
+            } // end if
+        
+            else {
+                System.out.println("Expression " + s[i] + " is not balanced\n");
+            } // end else
+            
+        } // end for
+
+} // end main
+    
+}

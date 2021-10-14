e757d2fe1b7c0e3ee6269e9eb8ce89320c01cef7
James Holmes <48929378+J-Holmes@users.noreply.github.com>
2021-10-14 09:21:00 -0400

    Add files via upload

---
+++ Project 1/Constants.java
@@ -0,0 +1,14 @@
+package app;
+
+public interface Constants {
+    public static final char 
+            LEFT_NORMAL = '(',
+            RIGHT_NORMAL = ')',
+            A_SPACE = ' ',
+            MULTIPLICATION = '*',
+            DIVISION = '/',
+            ADDITION = '+',
+            SUBTRACTION = '-';
+            
+            
+}
